bb3397817d6de70903d4571aabf57f71d434b17b
jakhingir611 <khurramovjahongir611@gmail.com>
2023-11-23 09:36:47 +0500

    супер

--- js/script.js
+++ js/script.js
@@ -1,9 +1,18 @@
 let doc = document;
 let body = doc.body;
+let main = doc.querySelector('.main');
 let btn_tel = doc.querySelector('.tel');
 let btn_pc = doc.querySelector('.pc');
 let market_phone = doc.querySelector('.market_phone');
 let market_pc = doc.querySelector('.market_pc');
+let catalog = doc.querySelector('.catalog');
+let vibor = doc.querySelector('.vibor');
+let tel = doc.querySelector('.tel');
+let pc = doc.querySelector('.pc');
+let izbr = doc.querySelector('.izbr');
+let like = doc.querySelector('.like');
+let like_fovarite = doc.querySelector('.like_fovarite');
+
 
 btn_tel.addEventListener('click', () => {
     market_pc.classList.add('none');
@@ -16,4 +25,17 @@ btn_pc.addEventListener('click', () => {
     market_pc.classList.remove('none');
     btn_pc.classList.add('lol');
     btn_tel.classList.remove('lol');
+})
+catalog.addEventListener('click', () => {
+    vibor.classList.remove('none');
+})
+tel.addEventListener('click', () => {
+    vibor.classList.add('none');
+})
+pc.addEventListener('click', () => {
+    vibor.classList.add('none');
+})
+like.addEventListener('click', () => {
+    main.classList.add('none');
+    izbr.classList.remove('none');
 })
